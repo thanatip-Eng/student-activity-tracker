945a1ef3acf4e8080aeb4b3ef75b0fd5a2ac0af4
Thanatip <thanatip@eng.cmu.ac.th>
2026-01-02 13:41:23 +0700

    Add files via upload

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -2,12 +2,12 @@
 // Go to Firebase Console > Project Settings > Your apps > Firebase SDK snippet
 
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyANurmMx-jye_Jqf6x63Y6oAO1903D7yy4",
+  authDomain: "student-activity-tracker-13eb5.firebaseapp.com",
+  projectId: "student-activity-tracker-13eb5",
+  storageBucket: "student-activity-tracker-13eb5.firebasestorage.app",
+  messagingSenderId: "142207116884",
+  appId: "1:142207116884:web:7bf08528f01674c61aefc1"
 };
 
 // Initialize Firebase
