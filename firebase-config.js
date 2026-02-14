c0060ed61c0fe4b8bf7aeb872027756f0f4e76e1
Thanatip <thanatip@eng.cmu.ac.th>
2026-02-14 21:12:34 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -4,7 +4,7 @@
 const firebaseConfig = {
   apiKey: "AIzaSyANurmMx-jye_Jqf6x63Y6oAO1903D7yy4",
   authDomain: "student-activity-tracker-13eb5.firebaseapp.com",
-  projectId: "student-activity-tracker-13eb5",
+  projectId: "gen-lang-client-0452928599",
   storageBucket: "student-activity-tracker-13eb5.firebasestorage.app",
   messagingSenderId: "142207116884",
   appId: "1:142207116884:web:7bf08528f01674c61aefc1"
